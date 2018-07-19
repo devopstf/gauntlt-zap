7c9190312ba4d55422520a29d2609f38b03e7a18
Antoine <amfumero@gmail.com>
2018-07-19 23:07:06 +0100

    fixing the problem with docker volumes

--- zap-docker.sh
+++ zap-docker.sh
@@ -13,7 +13,7 @@ fi
 ZAP_API_PORT=2375
 
 # starting ZAP daemon listening on port 2375
-CONTAINER_ID=$(docker run -u zap -p $ZAP_API_PORT:$ZAP_API_PORT -v $(pwd):/zap:rw -d owasp/zap2docker-weekly zap.sh -daemon -port $ZAP_API_PORT -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
+CONTAINER_ID=$(docker run -u zap -p $ZAP_API_PORT:$ZAP_API_PORT -v $(pwd):/zap/working:rw -d owasp/zap2docker-weekly zap.sh -daemon -port $ZAP_API_PORT -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
 
 # the target URL for ZAP to scan
 TARGET_URL=$1
@@ -34,7 +34,7 @@ docker exec $CONTAINER_ID zap-cli -v -p $ZAP_API_PORT active-scan -r $TARGET_URL
 docker exec $CONTAINER_ID zap-cli -p $ZAP_API_PORT alerts
 
 # generating the report in HTML format
-docker exec $CONTAINER_ID zap-cli -p $ZAP_API_PORT report -f html -o zap-report.html
+docker exec $CONTAINER_ID zap-cli -p $ZAP_API_PORT report -f html -o /zap/working/zap-report.html
 
 # gathering container logs
 docker logs $CONTAINER_ID >> zap.log
